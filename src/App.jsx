16ed92e9c6782e375b64932cef857ca611fac26b
Muhammad Haseeb Amjad <muhammadhaseebamjad.dev@example.com>
2025-11-20 22:53:38 +0500

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <h1>Api Calling using React Redux</h1>
       <button onClick={e => dispatch(fetchTodos())}>Fetch Todo's</button>
       {
-        state.todo.isData && state.todo.isData.map((e, i) => <li key={i}>{i + 1}{".     "}{e.title}</li>)
+        state.todo.isData && state.todo.isData.map((e, i) => <li key={i}>{i + 1}{". "}{e.title}</li>)
       }
     </>
   )
